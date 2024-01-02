158ea4dca5e031a849e130ad28ac675676cb1332
Szymon 128579 <pzx106226@student.poznan.merito.pl>
2024-01-02 21:08:16 +0100

    Dodano pliki

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zamowienia.Models
+{
+    public class Order : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
