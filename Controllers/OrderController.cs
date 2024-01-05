81824c641f3001ebcd5aeaee126e3f79ccfafb21
Szymon 128579 <pzx106226@student.poznan.merito.pl>
2024-01-05 23:20:50 +0100

    Sending form to database is working now.

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -42,60 +42,37 @@ public class OrderController : Controller
     {
         if (!ModelState.IsValid)
         {
-            var selectedProductIds = orderForm.Produkty
-                .Where(p => p.IsSelected)
-                .Select(p => p.Id)
-                .ToList();
-
-            if (!selectedProductIds.Any())
-            {
-                ModelState.AddModelError("", "Nie wybrano żadnych produktów.");
-                return View(orderForm);
-            }
-
-            if (_context.Przedmioty == null)
-            {
-                return View("Error", "Błąd: Brak dostępu do Przedmiotów w bazie danych.");
-            }
-
-            var selectedProducts = _context.Przedmioty
-                .Where(p => selectedProductIds.Contains(p.Id))
-                .ToList();
-
-            var zamowienie = new Order
-            {
-                dataZlozenia = DateTime.Now,
-                listaPrzedmiotow = string.Join(", ", selectedProducts.Select(p => p.NazwaProduktu)),
-                pracownikId = 1,
-                czyZrealizowano = "NIE",
-                dataRealizacji = DateTime.Now,
-                uwagi = orderForm.uwagi
-            };
-
-            if (_context.Zamowienia == null)
-            {
-                return View("Error", "Błąd: Brak dostępu do Zamówień w bazie danych.");
-            }
-
-            _context.Zamowienia.Add(zamowienie);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("OrderSuccess");
+            return View(orderForm);
         }
 
-        // Ponowne ładowanie produktów w przypadku błędu walidacji
-        if (_context.Przedmioty != null)
+        var selectedProducts = orderForm.Produkty
+            .Where(p => p.IsSelected)
+            .Select(p => p.NazwaProduktu)
+            .ToList();
+
+        if (!selectedProducts.Any())
         {
-            var produktyZBazy = await _context.Przedmioty.ToListAsync();
-            orderForm.Produkty = produktyZBazy.Select(p => new PrzedmiotViewModel
-            {
-                Id = p.Id,
-                NazwaProduktu = p.NazwaProduktu,
-                IsSelected = orderForm.Produkty.Any(x => x.Id == p.Id && x.IsSelected)
-            }).ToList();
+            ModelState.AddModelError("", "Nie wybrano żadnych produktów.");
+            return View(orderForm);
         }
 
-        return View(orderForm);
+        // Tworzenie nowego zamówienia
+        var zamowienie = new Order
+        {
+            dataZlozenia = DateTime.Now,
+            listaPrzedmiotow = string.Join(", ", selectedProducts),
+            pracownikId = 1, // przykładowy ID pracownika
+            czyZrealizowano = "NIE",
+            dataRealizacji = DateTime.Now,
+            uwagi = orderForm.uwagi
+        };
+
+        _context.Zamowienia.Add(zamowienie);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("OrderSuccess");
     }
+
 
     [HttpGet]
     public IActionResult OrderSuccess()
